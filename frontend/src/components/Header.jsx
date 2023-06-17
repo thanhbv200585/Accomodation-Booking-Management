d64df8b18eff05990f5b087679e30c3cc353f476
Nguyen Hai Son <son.nh205168@sis.hust.edu.vn>
2023-06-17 16:40:08 +0700

    update nodemodules

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+    <div className="header">
+        <header className="header">
+            <div className="logo">
+                <Link to="/">
+                    {<img src="" />}
+                </Link>
+            </div>
+        </header>
+    </div>
+}
+
