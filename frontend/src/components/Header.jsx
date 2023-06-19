0add6ed382e8e107b968533d64b56eb21966d4d2
coderXomDuong5 <Thckpck11@gmail.com>
2023-06-19 08:27:46 +0700

    Add header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from '../assets/img/logo.svg';
 const Header = () => {
-    <div className="header">
-        <header className="header">
-            <div className="logo">
-                <Link to="/">
-                    {<img src="" />}
-                </Link>
-            </div>
-        </header>
-    </div>
+    return (
+        <div className="header">
+            <header className="header">
+                <div className="navbar">
+                    <div className="logo">
+                        <Link to="/" style={{color: "#fff", height: "100px"}}>
+                            <img src={logo}  style={{width: '100px', height: '21px'}} alt="Booking.com"/>
+                        </Link>
+                    </div>
+                    <div className="sign-in">
+                        <ul className="sign-in">
+                            <li>
+                                <button className="theme"><a href="/">List your property</a></button>
+                            </li>
+                            <li>
+                                <button className="dark"><a href="/register">Register</a></button>
+                            </li>
+                            <li>
+                                <button className="dark"><a href="/login">Sign in</a></button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+    
 }
+
+export default Header;
 
