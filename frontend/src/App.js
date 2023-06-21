94a93a40f65f3a0636bcbc83087d167ebc4676e1
Nguyen Hai Son <son.nh205168@sis.hust.edu.vn>
2023-06-22 02:58:28 +0700

    son1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header"
 function App() {
   return (
     <div className="App">
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/api/guest/register" element={<RegisterPage />} />
         </Routes>
       </Router>
     </div>
